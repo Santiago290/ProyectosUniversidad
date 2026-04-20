9c4e5d6443c6ec44d08f77a281444a1297cad464
Santiago Deza <santi_pro14@hotmail.com>
2026-04-20 11:45:19 -0500

    cambios iniciales

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProyectosUniversidad.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
